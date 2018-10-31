90bc7faf8c817d55fcdb61d3d417df6083222b31
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-10-31 06:37:38 +0100

    Test passes.

---
+++ mopyx/__init__.py
@@ -0,0 +1,78 @@
+from typing import Callable, TypeVar, List, Dict, Set
+import functools
+
+T = TypeVar('T')
+
+
+class RendererFunction:
+    def __init__(self, f) -> None:
+        self.callable = f
+
+
+active_renderers: List[RendererFunction] = list()
+
+
+def render(f: Callable[..., T]) -> Callable[..., T]:
+    @functools.wraps(f)
+    def wrapper(*args, **kw) -> T:
+        try:
+            renderer = RendererFunction(f)
+            active_renderers.append(renderer)
+            return f(*args, **kw)
+        finally:
+            active_renderers.pop()
+
+    return wrapper
+
+
+class ModelProxy(object):
+    """
+    Tracks who was rendered from what properties.
+    """
+
+    def __init__(self, target):
+        self._mopyx_target = target
+        self._mopyx_renderers: Dict[str, Set[RendererFunction]] = dict()
+
+    def __getattr__(self, name):
+        if active_renderers:
+            renderers = self._mopyx_renderers.get(name, None)
+
+            if not renderers:
+                renderers = set()
+                self._mopyx_renderers[name] = renderers
+
+            renderers.add(active_renderers[-1])
+
+        return self._mopyx_target.__getattribute__(name)
+
+    def __setattr__(self, name, value):
+        if name.startswith("_mopyx"):
+            super().__setattr__(name, value)
+            return
+
+        setattr(self._mopyx_target, name, value)
+
+        renderers = self._mopyx_renderers.get(name, None)
+        if renderers:
+            for renderer in renderers:
+                renderer.callable()
+
+
+def model(f: Callable[..., T]) -> Callable[..., T]:
+    @functools.wraps(f)
+    def wrapper(*args, **kw) -> T:
+        result = f(*args, **kw)
+
+        return ModelProxy(result)
+
+    return wrapper
+
+
+def render_call(f: Callable[..., T]) -> T:
+    @render
+    def internal_render():
+        return f()
+
+    return internal_render()
+
---
+++ test/test_render.py
@@ -0,0 +1,73 @@
+import unittest
+
+from mopyx import render, render_call, model
+
+
+@model
+class RootModel:
+    def __init__(self):
+        self.name = "initial name"
+        self.desc = "initial description"
+        self.title = "initial title"
+
+
+registered_events = []
+
+
+class UiLabel:
+    def __init__(self):
+        self.label = None
+
+    def set_label(self, label):
+        registered_events.append("UiLabel.set_label")
+        self.label = label
+
+
+class UiComponent:
+    def __init__(self, model):
+        self.model = model
+
+        self.name = UiLabel()
+        self.description = UiLabel()
+        self.title = None
+
+        self.update_data()
+
+    @render
+    def update_data(self):
+        registered_events.append("UiComponent.update_data")
+
+        render_call(lambda: self.name.set_label(self.model.name))
+        render_call(lambda: self.description.set_label(self.model.desc))
+
+        self.title = self.model.title
+
+
+class TestRender(unittest.TestCase):
+    """
+    Test the @render decorator.
+    """
+
+    def test_render_function(self):
+        """
+        Will create a test rendering, and change properties.
+        """
+        model = RootModel()
+        ui = UiComponent(model)
+
+        self.assertEqual(model.name, ui.name.label)
+        self.assertEqual(model.desc, ui.description.label)
+
+        model.name = "updated name"
+        model.desc = "updated description"
+
+        self.assertEqual(model.name, "updated name")
+        self.assertEqual(model.desc, "updated description")
+
+        self.assertEqual(model.name, ui.name.label)
+        self.assertEqual(model.desc, ui.description.label)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
