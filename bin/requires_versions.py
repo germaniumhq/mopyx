8813ba8522871275b8762dad94011b8c3f6b7479
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-11-27 09:06:50 +0100

    Use adhesive for the builds

--- bin/requires_versions.py
+++ bin/requires_versions.py
@@ -3,7 +3,8 @@
 install_requires = []
 with open('requirements.txt', mode='r') as requirements_file:
     for line in requirements_file.readlines():
-        if line and not line.startswith('#'):
+        line = line.strip()
+        if line and not line.startswith('#') and not line.endswith('# optional'):
             install_requires.append('        "%s"' % line.strip())
 
 print("\n" + ",\n".join(install_requires))
